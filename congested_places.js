90b3e999fe1b317ae171c2f5e7ad9d4f80e92437
masyiaulia <masyithahaulia@gmail.com>
2018-11-18 23:24:14 +0700

    create daerah terpadat, jam terpadat dan comparison sum congestion vs dist jkt-bdg

---
+++ congested_places.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    var chart;
+	
+    $(document).ready(function() {
+       	var jqxhr = $.getJSON("https://raw.githubusercontent.com/anianisyah/visdat/master/recap_most_traffic_street.json", function(data) {
+            var places = [];
+            var jamFactors = [];
+            var colors = Highcharts.getOptions().colors;
+            for (var i = 0; i < jqxhr.responseJSON.length; i++){
+            	if(jqxhr.responseJSON[i]["jam_factor"] > 1){
+            		places.push(jqxhr.responseJSON[i]["street_name"]);
+            		jamFactors.push(jqxhr.responseJSON[i]["jam_factor"]);
+            		color: colors[i];
+            	}
+            }   
+
+            chart = new Highcharts.chart('container3', {
+                title: {
+                    text: 'Most Traffic Streets'
+                },
+                xAxis: {
+                    categories: places
+                },
+                yAxis: {
+                    title: {
+                        text: 'Jam Factors'
+                    }
+                },
+                tooltip: {
+                    formatter: function() {
+                        var point = this.point,
+                            s = this.x +'<br><b> Jam Factor: '+ this.y +'</b><br/>';
+                        return s;
+                    }
+                },
+                series: [{
+                    type: 'column',
+                    name: 'Streets',
+                    data: jamFactors,
+                    color: 'red'
+                }],
+                exporting: {
+                    enabled: false
+                }
+		    });  
+	    });
+	});
+})
