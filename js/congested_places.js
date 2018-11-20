2cc246a0c2fbe875ae3fd855fc139632af856fa5
Rendy <rendy.faqot@gmail.com>
2018-11-20 21:35:16 +0700

    Update congested_places.js

--- js/congested_places.js
+++ js/congested_places.js
@@ -24,7 +24,7 @@ $(function () {
                 },
                 yAxis: {
                     title: {
-                        text: 'Jam Factors'
+                        text: 'Percentage'
                     }
                 },
                 tooltip: {
